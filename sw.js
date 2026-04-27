a802ae0d4e0e4187b8d9244e3039a6d374e7b07f
AUSTINTRADEVENTURES <ezeaugustinemmaduabuchi@gmail.com>
2026-04-27 20:44:41 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", (e) => {
+    console.log("SW Installed");
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+    console.log("SW Activated");
+});
+
+self.addEventListener("fetch", (e) => {});
